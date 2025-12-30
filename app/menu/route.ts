2c36e616be1bce82fd5d72b1987af5698c9b8068
karim elsayed <kwsa1g20@soton.ac.uk>
2025-12-30 00:15:36 +0000

    menu reprint

--- app/menu/route.ts
+++ app/menu/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   // This is where the QR will redirect
   return NextResponse.redirect(
-    "https://www.canva.com/design/DAG5tST81Ns/1eUR9E5TLnI6U4BeX6SaBA/edit", // <-- change later
+    "https://drive.google.com/file/d/1yVWucBjAvb53xve-W6R7wfcxGlegbnTV/view?usp=drivesdk", // <-- change later
     { status: 302 }
   );
 }
